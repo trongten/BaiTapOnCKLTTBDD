d87df47bda3c0c07d253ee0a6f12cc40a6a54142
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2022-05-14 10:19:20 +0700

    bo sung them

--- app/src/main/java/com/example/movierating/activity/MainForLogin.java
+++ app/src/main/java/com/example/movierating/activity/MainForLogin.java
@@ -2,15 +2,30 @@ package com.example.movierating.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.movierating.R;
 
 public class MainForLogin extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main_for_login);
+
+        btnLogin = (Button) findViewById(R.id.btnToLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainForLogin.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
